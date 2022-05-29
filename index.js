31dc6b60aa02d70de61e4fc51d02ddac7282915e
thiagoss2000 <thiago.ss675@gmail.com>
2022-05-29 16:56:38 -0300

    games

--- index.js
+++ index.js
@@ -36,6 +36,51 @@ app.post('/categories', async (req, res) => {
     }
 })
 
+/////////////games////////////
+app.get('/games', async (req, res) => {
+    const name = req.query.name;
+    try {
+        let nameGame = null;
+        if (name){
+            const listGame = await db.query(`SELECT name FROM games`);
+            nameGame = listGame.rows.find(el => {
+                return el.name.substr(0, name.length).toUpperCase() == name.toUpperCase()       
+            })
+        }
+        const games = await db.query(`
+            SELECT games.*, categories.name as "categoryName"
+            FROM games
+            JOIN categories
+            ON games."categoryId" = categories.id        
+            ${(nameGame? `WHERE games.name = '${nameGame.name}'` : '')}
+        `);
+        res.status(200).send(games.rows);
+    } catch (e){
+        console.log(e);
+        res.sendStatus(400);
+    }
+})
+
+app.post('/games', async (req, res) => {
+    const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+    try {
+        const categorieId = await db.query(`SELECT id FROM categories`);
+        if (!name || stockTotal <= 0 || pricePerDay <= 0 || !categorieId.rows.some(el => el.id == categoryId)) 
+            return res.sendStatus(400);
+        const nameGames = await db.query(`SELECT name FROM games`);
+        if (nameGames.rows.some(a => a.name.toUpperCase() == name.toUpperCase())) 
+            return res.sendStatus(409);
+        await db.query(`INSERT INTO games 
+            (name, image, "stockTotal", "categoryId", "pricePerDay") 
+            VALUES ('${name}', '${image}', ${stockTotal}, ${categoryId}, ${pricePerDay})`);
+        console.log(nameGames.rows);
+        res.sendStatus(201);
+    } catch (e){
+        console.log(e);
+        res.sendStatus(400);
+    }
+})
+
 app.listen(process.env.PORT, () => {
     console.log("server on");
 })
